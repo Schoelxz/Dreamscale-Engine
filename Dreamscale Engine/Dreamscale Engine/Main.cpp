86dc166b03c88748c3a43aef18b529b7e173b8ea
Joel Åman <Schoelxz@gmail.com>
2017-04-24 10:23:53 +0200

    Tog Bort Grejer i Main (så som den kommer vara i denna comitten SKA den vara alltid i allas projekt (se slack))

--- Dreamscale Engine/Dreamscale Engine/Main.cpp
+++ Dreamscale Engine/Dreamscale Engine/Main.cpp
@@ -2,22 +2,11 @@
 //x86
 #include "GameEngine.h"
 
-
-
 int main()
 {
 	std::cout << "Dreamscale Engine" << std::endl;
 
 	GameEngine gameEngine;
-	gameEngine.~GameEngine();
-
-	gameEngine.project.SetFilename("myFirstProject1.txt");
-	gameEngine.project.SaveProject("This is my data in my project!!!");
-
-	if (gameEngine.project.FileExists("myFirstProject1.txt"))
-		std::cout << "File Exists!" << std::endl;
-
-	//Min Super FIna kommentar, jag gillar den så braaaaaaaaaa.
 
 	while (1); //dont use this. but use it anyway
 
