1fb5ed4b9753aa68ec3436272cea3176c860cbed
Kevin Holmgren <ketoho@hotmail.com>
2017-04-28 16:04:19 +0200

    include tmxparser

--- Dreamscale Engine/Dreamscale Engine/TmxHandler.h
+++ Dreamscale Engine/Dreamscale Engine/TmxHandler.h
@@ -1,4 +1,22 @@
 #pragma once
+// Include tmx-stuff we use in the program
+#include <Tmx\TmxMap.h>
+#include <Tmx\TmxObjectGroup.h>
+#include <Tmx\TmxObject.h>
+#include <Tmx\TmxTileset.h>
+#include <Tmx\TmxTileLayer.h>
+#include <Tmx\TmxImage.h>
+#include <Tmx\TmxTile.h>
+#include <Tmx\TmxMapTile.h>
+
+// Include part of the standard library
+#include <cassert>
+#include <iostream>
+#include <array>
+
+// Include sfml to render our layers
+#include <SFML\Graphics.hpp>
+
 class TmxHandler
 {
 public:
