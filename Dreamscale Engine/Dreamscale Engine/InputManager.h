610985e78f1c1a08cc305eb60c6112b426c20ec3
DANNES\Daniel <daniel_ulfenborg>
2017-05-16 13:08:24 +0200

    Början på input managern, måste dock förmodligen styras av en system manager i sitt tur, vet inte om det är den klassen som skall representeras av resourcehandler?

--- Dreamscale Engine/Dreamscale Engine/InputManager.h
+++ Dreamscale Engine/Dreamscale Engine/InputManager.h
@@ -6,5 +6,16 @@ class InputManager
 public:
 	InputManager();
 	~InputManager();
+
+	void Update();
+	
+private:
+	
+	bool wasKeyPressed(unsigned int key);
+	bool wasKeyReleased(unsigned int key);
+
+	bool isKeyDown(unsigned int key);
+	bool isKeyUp(unsigned int key);
+
 };
 
